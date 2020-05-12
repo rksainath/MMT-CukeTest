88235f06e718f0acdc3031664eacb8b4be0ffcc7
rksainath <amidstgeeks@gmail.com>
2020-05-13 01:26:30 +0530

    jen

--- src/test/java/commons/BrowserConfig.java
+++ src/test/java/commons/BrowserConfig.java
@@ -10,18 +10,18 @@ public class BrowserConfig {
 
 	public static WebDriver driver;
 	public String path = "src/test/resources/drivers/";
+	public static String chromepath = "/usr/local/bin/chromedriver";
 
 	public BrowserConfig(String browserName) {
 		String browser = browserName.toLowerCase();
 		switch (browser) {
 		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+			System.setProperty("webdriver.chrome.driver", chromepath);
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--headless");
 			options.addArguments("start-maximized"); // open Browser in maximized mode
 			options.addArguments("disable-infobars"); // disabling infobars
 			options.addArguments("--disable-extensions"); // disabling extensions
-			options.addArguments("--disable-gpu"); // applicable to windows os only
 			options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 			options.addArguments("--no-sandbox"); // Bypass OS security model
 			WebDriver driver = new ChromeDriver(options);
