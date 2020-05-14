63e00c5da56ba3786ba3ec74d855bacc23541b86
rksainath <amidstgeeks@gmail.com>
2020-05-14 11:04:45 +0530

    chrome option

--- src/test/java/commons/BrowserConfig.java
+++ src/test/java/commons/BrowserConfig.java
@@ -29,14 +29,14 @@ public class BrowserConfig {
 			ChromeOptions options = new ChromeOptions();
 //			options.setBinary("/usr/bin/google-chrome-stable");
 //			options.addArguments("--window-size=1920,1080");
-//			options.addArguments("--disable-extensions");
+			options.addArguments("--disable-extensions");
 //			options.addArguments("--proxy-server='direct://'");
 //			options.addArguments("--proxy-bypass-list=*");
 			options.addArguments("--start-maximized");
 //			options.addArguments("--headless");
 //			options.addArguments("--disable-gpu");
 //			options.addArguments("--disable-dev-shm-usage");
-//			options.addArguments("--no-sandbox");
+			options.addArguments("--no-sandbox");
 			options.addArguments("--ignore-certificate-errors");
 //			
 			try {
